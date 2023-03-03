c5c575ef3d842601b4bb4b0e5873b32e837fda51
Ana Luiza Rodrigues Silveira <anarodri@c2r2p12.42quebec.com>
2023-03-03 13:10:27 -0500

    Rotation formulas (look_right and look_left) corrected! Next step: Fix movement (forward, backward, strafe).

--- src/engine/move.c
+++ src/engine/move.c
@@ -1,46 +1,46 @@
 #include "../include/cub3d.h"
 
 void	look_left(t_ray *rc)
-// {
-// 	double old_dir_x;
-// 	double old_plane_x;
-
-// 	old_dir_x = rc->dir_x;
-// 	old_plane_x = rc->plane_x;
-
-// 	rc->dir_x = rc->dir_x * cos(-rc->rot_speed) - rc->dir_x * sin(-rc->rot_speed);
-// 	rc->dir_y = old_dir_x * sin (-rc->rot_speed) + rc->dir_y * cos (-rc->rot_speed);
-// 	rc->plane_x = rc->plane_x * cos(-rc->rot_speed) - rc->plane_x * sin(-rc->rot_speed);
-// 	rc->plane_y = old_plane_x * sin (-rc->rot_speed) + rc->plane_y * cos (-rc->rot_speed);
-// }
 {
-	rc->angle -= 0.1;
-	if (rc->angle < 0)
-		rc->angle += (2 * M_PI);
-	rc->dir_x = cos(rc->angle) * 2 * (rc->rot_speed + 0.5);
-	rc->dir_y = sin(rc->angle) * 2 * (rc->rot_speed + 0.5);
+	double old_dir_x;
+	double old_plane_x;
+
+	old_dir_x = rc->dir_x;
+	old_plane_x = rc->plane_x;
+
+	rc->dir_x = rc->dir_x * cos(-rc->rot_speed) - rc->dir_y * sin(-rc->rot_speed);
+	rc->dir_y = old_dir_x * sin (-rc->rot_speed) + rc->dir_y * cos (-rc->rot_speed);
+	rc->plane_x = rc->plane_x * cos(-rc->rot_speed) - rc->plane_y * sin(-rc->rot_speed);
+	rc->plane_y = old_plane_x * sin (-rc->rot_speed) + rc->plane_y * cos (-rc->rot_speed);
 }
+// {
+// 	rc->angle -= 0.1;
+// 	if (rc->angle < 0)
+// 		rc->angle += (2 * M_PI);
+// 	rc->dir_x = cos(rc->angle) * 2 * (rc->rot_speed + 0.5);
+// 	rc->dir_y = sin(rc->angle) * 2 * (rc->rot_speed + 0.5);
+// }
 
 void	look_right(t_ray *rc)
-// {
-// 	double old_dir_x;
-// 	double old_plane_x;
-
-// 	old_dir_x = rc->dir_x;
-// 	old_plane_x = rc->plane_x;
-
-// 	rc->dir_x = rc->dir_x * cos(rc->rot_speed) - rc->dir_x * sin(rc->rot_speed);
-// 	rc->dir_y = old_dir_x * sin (rc->rot_speed) + rc->dir_y * cos (rc->rot_speed);
-// 	rc->plane_x = rc->plane_x * cos(rc->rot_speed) - rc->plane_x * sin(rc->rot_speed);
-// 	rc->plane_y = old_plane_x * sin (rc->rot_speed) + rc->plane_y * cos (rc->rot_speed);
-// }
 {
-	rc->angle += 0.1;
-	if (rc->angle > (2 * M_PI))
-		rc->angle -= (2 * M_PI);
-	rc->dir_x = cos(rc->angle) * 2 * (rc->rot_speed + 0.5);
-	rc->dir_y = sin(rc->angle) * 2 * (rc->rot_speed + 0.5);
+	double old_dir_x;
+	double old_plane_x;
+
+	old_dir_x = rc->dir_x;
+	old_plane_x = rc->plane_x;
+
+	rc->dir_x = (rc->dir_x * cos(rc->rot_speed)) - (rc->dir_y * sin(rc->rot_speed));
+	rc->dir_y = (old_dir_x * sin (rc->rot_speed)) + (rc->dir_y * cos (rc->rot_speed));
+	rc->plane_x = (rc->plane_x * cos(rc->rot_speed)) - (rc->plane_y * sin(rc->rot_speed));
+	rc->plane_y = (old_plane_x * sin (rc->rot_speed)) + (rc->plane_y * cos (rc->rot_speed));
 }
+// {
+// 	rc->angle += 0.1;
+// 	if (rc->angle > (2 * M_PI))
+// 		rc->angle -= (2 * M_PI);
+// 	rc->dir_x = cos(rc->angle) * 2 * (rc->rot_speed + 0.5);
+// 	rc->dir_y = sin(rc->angle) * 2 * (rc->rot_speed + 0.5);
+// }
 
 void	strafe(t_ray *rc, char dir)
 {
