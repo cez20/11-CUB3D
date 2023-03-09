90cf4809b850ab2a9e0b5da3aff1d2dea6759451
Cesar Menjivar <cemenjiv@student.42quebec.com>
2023-03-09 15:56:03 -0500

    Added explanation to my function create_2D_array

--- src/engine/texture.c
+++ src/engine/texture.c
@@ -1,52 +1,28 @@
 #include "../include/cub3d.h"
 
 
-// int	**fill_texture(xpm_t *tex)
-// {
-// 	int	**arr;
-// 	int	i;
-// 	int	j;
-
-// 	arr = ft_calloc(sizeof(int *), tex->texture.height + 1);
-// 	i = 3;
-// 	while (++i < (int) tex->texture.height + 4)
-// 	{
-// 		j = 3;
-// 		arr[i - 4] = ft_calloc(sizeof(int), tex->texture.width);
-// 		while (++j < (int) tex->texture.width + 4)
-// 			arr[i - 4][j - 4] = create_colour(
-// 					tex->texture.pixels[(tex->texture.width * 4
-// 						* (i - 4)) + (4 * (j - 4)) + 0],
-// 					tex->texture.pixels[(tex->texture.width * 4
-// 						* (i - 4)) + (4 * (j - 4)) + 1],
-// 					tex->texture.pixels[(tex->texture.width * 4
-// 						* (i - 4)) + (4 * (j - 4)) + 2],
-// 					tex->texture.pixels[(tex->texture.width * 4
-// 						* (i - 4)) + (4 * (j - 4)) + 3]);
-// 	}
-// 	return (arr);
-// }
-
-
+//This function createa a 2D array for textures based on width and height (64 X 64)
+//This functions calls the fonction get_color and transform the RGBA value into an int.
+// pixel[0] = R value, pixel[1] = G value, pixel[2] = 'B' value  pixel[3] = A value 
 int	**create_2D_array_textures(xpm_t *image)
 {
 	int **tab;
 	int i;
 	int j;
 
-	tab = ft_calloc(sizeof(int *), image->texture.height + 1);
+	tab = ft_calloc(sizeof(int *), image->texture.height); // Maybe we should put height + 1 but not necessary 
 	i = 3;
 	while (++i < (int)image->texture.height + 4)
 	{	
 		j = 3;
-		tab[i - 4] =  ft_calloc(sizeof(int), image->texture.width + 4);
+		tab[i - 4] =  ft_calloc(sizeof(int), image->texture.width + 4); // je crois qu,on pourrait enlever le + 4
 		while (++j < (int) image->texture.width)
 		{
 			tab[i - 4][j - 4] = get_color(
 					image->texture.pixels[(image->texture.width * 4 * (i - 4)) + (4 * (j - 4)) + 0], 
 					image->texture.pixels[(image->texture.width * 4 * (i - 4)) + (4 * (j - 4)) + 1],
 					image->texture.pixels[(image->texture.width * 4 * (i - 4)) + (4 * (j - 4)) + 2], 
-					image->texture.pixels[(image->texture.width * 4 * (i - 4)) + (4 * (j - 4)) + 3];
+					image->texture.pixels[(image->texture.width * 4 * (i - 4)) + (4 * (j - 4)) + 3]);
 		}
 	}
 	return (tab);
@@ -67,5 +43,8 @@ void 	load_xpm42_textures(t_game *g)
 	if (!g->tex->ea)
 		errmsg("Problem with loading xmp42 images", 1, g); // Je dois aussi free tout ce qui concerne mlx
 	g->tex->n = create_2D_array_textures(g->tex->no);
+	g->tex->s = create_2D_array_textures(g->tex->so);
+	g->tex->e = create_2D_array_textures(g->tex->ea);
+	g->tex->w = create_2D_array_textures(g->tex->we);
 
 }
